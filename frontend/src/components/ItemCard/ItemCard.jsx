ad5cd4927e5153aa3cc77027934e9b956948048d
Felipe Botero <frpbotero@gmail.com>
2022-11-23 20:29:48 -0400

    useState

--- frontend/src/components/ItemCard/ItemCard.jsx
+++ frontend/src/components/ItemCard/ItemCard.jsx
@@ -3,11 +3,11 @@ const url = "https://ocean-jn-fullstack-22fb.onrender.com/Itens"
 
 function ItemCard(props){
     const item = props.item
-    console.log(item.nome)
+    
     return(
         <div className="ItemCard">
             <h1>{item.nome}</h1>
-            <img src={item.imageUrl} alt={"Imagem do " + item.nome} />
+            <img src={item.imgUrl} alt={"Imagem do " + item.nome} />
         </div>
     )
 }
--- frontend/src/components/ReadAll/ReadAll.jsx
+++ frontend/src/components/ReadAll/ReadAll.jsx
@@ -1,36 +1,45 @@
+import { useState } from "react"
 import { Api } from "../../api/api"
 import ItemCard from "../ItemCard/ItemCard"
+import carregando from "../../assets/carregando.gif"
 import "./ReadAll.css"
-function ReadAll(){
-    const itens = [
-        {
-            _id:"123",
-            nome:"Cafe",
-            imageUrl:"https://picsum.photos/200/200"
-            },
-            {
-            _id:"233",
-            nome:"bala",
-            imageUrl:"https://picsum.photos/200/300"
-            },
-            {
-            _id:"233",
-            nome:"bala",
-            imageUrl:"https://picsum.photos/200/300"
-            }
-            ,
-            {
-            _id:"233",
-            nome:"bala",
-            imageUrl:"https://picsum.photos/200/300"
+ function ReadAll(){
+    // const itens = [
+    //     {
+    //         _id:"123",
+    //         nome:"Cafe",
+    //         imageUrl:"https://picsum.photos/200/200"
+    //         },
+    //         {
+    //         _id:"233",
+    //         nome:"bala",
+    //         imageUrl:"https://picsum.photos/200/300"
+    //         }
+   
+    //     ]
+
+        // Desconstrução de array
+            const [itens,setItens] = useState()
+
+            async function realizarRequisicao(){
+            
+            
+            const readAllUrl = Api.itens.readAll();
+            
+            const response = await Api.buildApiGetRequest(readAllUrl);
+            
+            const resultado = await response.json();
+            
+            setItens(resultado);
+            
             }
             
+            realizarRequisicao();
 
-
-    ]
-            const readAllUrl = Api.itens.readAll();
-            Api.buildApiGetRequest(readAllUrl)
-
+            if(itens ===undefined){
+                return <div><img src={carregando} alt="" /></div>
+            }
+            
 
     return(
         <div className="ReadAll">
